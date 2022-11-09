75babdf9b1d169ee31bea21a0cc8eeeac2bf8e38
aldaine-AG <aldaineclarke1@gmail.com>
2022-11-09 15:38:16 -0500

    request products from database

--- lib/navigation/directory_page.dart
+++ lib/navigation/directory_page.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 
 import 'package:farmers_directory/models/farmer.model.dart';
+import 'package:farmers_directory/pages/auth/login_page.dart';
 import 'package:farmers_directory/services/network_handler_service.dart';
 import 'package:farmers_directory/utils/functions.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
+import '../models/product.model.dart';
 import '../pages/users/lists/farmers_list.dart';
 import '../utils/dimensions.dart';
 import '../widgets/lg_text.dart';
@@ -20,18 +20,30 @@ class Directory extends StatefulWidget {
 
 class _DirectoryState extends State<Directory> {
   late Future<List<Farmer>> farmerList;
+  List<Product> products = [];
 
-  getFarmers() async{
+  Future<List<Farmer>> getFarmers() async{
       Map<String, dynamic> response = jsonDecode(await NetworkHandler.get(endpoint: "/farmers"));
       List farmers = response["data"];
-      return farmers.map((farmer){
+      List<Farmer> farmerList = farmers.map((farmer){
         return Farmer.fromJson(farmer);
       }).toList();
+      return farmerList;
+  }
+  getProducts() async{
+    Map<String, dynamic> response = jsonDecode(await NetworkHandler.get(endpoint:"/products"));
+    List productsList = response["data"];
+    setState((){
+      products = productsList.map((product){
+        return Product.fromJson(product);
+      }).toList();
+    });
   }
 
   @override
   void initState() {
     super.initState();
+    getProducts();
     farmerList = getFarmers();
   }
 
@@ -92,7 +104,7 @@ class _DirectoryState extends State<Directory> {
             }else if(snapshot.connectionState == ConnectionState.waiting){
               return Center(child: CircularProgressIndicator());
             }else{
-              return Center();
+              return showError(snapshot.error, farmerList, setState, getFarmers,context);
             }
         },
 
