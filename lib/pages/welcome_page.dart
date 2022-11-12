9ad3c2b524101b272a39c7378510d6b614244e3d
aldaine-AG <aldaineclarke1@gmail.com>
2022-11-12 02:59:26 -0500

    changed the initial page on appload to be welcome page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../navigation/home_page.dart';
+import 'auth/login_page.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -20,75 +21,79 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          ClipPath(
-            clipper: CustomClip(),
-            child: Container(
-              height: Dimensions.welcomePageImg,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    'assets/images/welcome_page.jpeg',
+      body: Builder(
+        builder: (context) {
+          return Column(
+            children: [
+              ClipPath(
+                clipper: CustomClip(),
+                child: Container(
+                  height: Dimensions.welcomePageImg,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/images/welcome_page.jpeg',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: Dimensions.height20),
-              width: double.maxFinite,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    RichText(
-                      text: TextSpan(
-                        text: 'Get Connected'.toUpperCase(),
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 35,
-                            fontWeight: FontWeight.w600),
-                        children: [
-                          TextSpan(
-                              text: '\nWith Real Farmers'.toUpperCase(),
-                              style: TextStyle(fontSize: 28)),
-                        ],
-                      ),
-                    ),
-                    LargeText(
-                      text:
-                          "From the farm to you yaad, cut down\nthird-party costs.",
-                      align: TextAlign.center,
-                    ),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                          backgroundColor: Color(0xFF8CB369),
-                          shape: StadiumBorder()),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => HomePage(),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: Dimensions.height20),
+                  width: double.maxFinite,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: 'Get Connected'.toUpperCase(),
+                            style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 35,
+                                fontWeight: FontWeight.w600),
+                            children: [
+                              TextSpan(
+                                  text: '\nWith Real Farmers'.toUpperCase(),
+                                  style: TextStyle(fontSize: 28)),
+                            ],
                           ),
-                        );
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: Dimensions.height15,
-                            horizontal: Dimensions.width15),
-                        child: LargeText(
-                          text: 'Get Started',
-                          color: Colors.white,
-                          size: Dimensions.height20,
                         ),
-                      ),
-                    ),
-                  ]),
-            ),
-          )
-        ],
+                        LargeText(
+                          text:
+                              "From the farm to you yaad, cut down\nthird-party costs.",
+                          align: TextAlign.center,
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor: Color(0xFF8CB369),
+                              shape: StadiumBorder()),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LoginPage(),
+                              ),
+                            );
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: Dimensions.height15,
+                                horizontal: Dimensions.width15),
+                            child: LargeText(
+                              text: 'Get Started',
+                              color: Colors.white,
+                              size: Dimensions.height20,
+                            ),
+                          ),
+                        ),
+                      ]),
+                ),
+              )
+            ],
+          );
+        }
       ),
     );
   }
