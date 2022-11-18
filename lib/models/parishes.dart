bbdef5f81f99b76428417efd467953a5c3c737c6
aldaine-AG <aldaineclarke1@gmail.com>
2022-11-18 01:12:58 -0500

    added dropdown for parishes using a static list

---
+++ lib/models/parishes.dart
@@ -0,0 +1,17 @@
+List<String> parishList = [
+  "Kingston",
+  "St. Andrew",
+  "St. Catherine",
+  "St. Elizabeth",
+  "St. Ann",
+  "Clarendon",
+  "Manchester",
+  "Westmoreland",
+  "St Thomas",
+  "Portland",
+  "St. Mary",
+  "St. James",
+  "Hanover",
+  "Trelawny",
+];
+
--- lib/pages/auth/signup_page.dart
+++ lib/pages/auth/signup_page.dart
@@ -5,6 +5,7 @@ import 'package:farmers_directory/widgets/sm_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import '../../models/parishes.dart';
 import '../../models/user.model.dart';
 import '../../utils/colors.dart';
 import '../../widgets/text_field.dart';
@@ -72,164 +73,252 @@ class _SignUpPageState extends State<SignUpPage> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    parishCtrl.text = parishList[0];
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: (!_loading) ? Stepper(
-        controlsBuilder: (context, steps){
-          return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: isLastStep ? Row(
-                children: [
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: AppColors.mainGreen,
-                      shape: StadiumBorder(),
-                    ),
-                    onPressed: () {
-                      submitSignUp();
-                    },
-                    child: LargeText(
-                      text: 'Submit',
-                      color: Colors.white,
-                    ),
-                  ),
-                  TextButton(onPressed: steps.onStepCancel, child: const Text("Cancel", style: TextStyle(color: Colors.white70),))
-                ],
-              ) : Row(
-                children: [
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: AppColors.mainGreen,
-                      shape: StadiumBorder(),
-                    ),
-                    onPressed: steps.onStepContinue,
-                    child: LargeText(
-                      text: 'Next',
-                      color: Colors.white,
-                    ),
-                  ),
-                  TextButton(onPressed: steps.onStepCancel, child: const Text("Cancel", style: TextStyle(color: AppColors.mainGreen),))
-                ],
-              )
-          );
-        },
+      resizeToAvoidBottomInset: false,
 
-        // type: StepperType.horizontal,
-        currentStep: _index,
-        onStepCancel: () {
-          if (_index > 0) {
-            setState(() {
-              _index -= 1;
-            });
-          }
-        },
-        onStepContinue: () {
-          if (_index <= 0 ) {
-            _index += 1;
-          }
-          if(_index == 1){
-            isLastStep = true;
-
-          }
-          setState(() {});
-
-        },
-        onStepTapped: (int index) {
-          if(index != 1){
-            isLastStep = false;
-          }else{
-            isLastStep = true;
-          }
-          setState(() {
-            _index = index;
-          });
-        },
-        steps: [
-          Step(
-
-            title: SmallText(text: 'Personal Details'),
-            content: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: (!_loading) ? Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: ListView(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 2.7,
-                      child: CustomTextField(
-                        controller: first_nameCtrl,
-                        title: 'First Name',
-                        placeholder: 'John',
+                SizedBox(
+                  height: 30
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:40.0),
+                  child: Text("Create an Account", style: TextStyle(color: AppColors.mainGreen, fontWeight: FontWeight.w600, fontSize: 25),),
+                ),
+                Stepper(
+                  controlsBuilder: (context, steps){
+                    return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: isLastStep ? Row(
+                          children: [
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                backgroundColor: AppColors.mainGreen,
+                                shape: StadiumBorder(),
+                              ),
+                              onPressed: () {
+                                submitSignUp();
+                              },
+                              child: LargeText(
+                                text: 'Submit',
+                                color: Colors.white,
+                              ),
+                            ),
+                            TextButton(onPressed: steps.onStepCancel, child: const Text("Cancel", style: TextStyle(color: AppColors.mainGreen),))
+                          ],
+                        ) : Row(
+                          children: [
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                backgroundColor: AppColors.mainGreen,
+                                shape: StadiumBorder(),
+                              ),
+                              onPressed: steps.onStepContinue,
+                              child: LargeText(
+                                text: 'Next',
+                                color: Colors.white,
+                              ),
+                            ),
+                            TextButton(onPressed: steps.onStepCancel, child: const Text("Cancel", style: TextStyle(color: AppColors.mainGreen),))
+                          ],
+                        )
+                    );
+                  },
+
+
+                  // type: StepperType.horizontal,
+                  currentStep: _index,
+                  onStepCancel: () {
+                    if (_index > 0) {
+                      setState(() {
+                        _index -= 1;
+                      });
+                    }
+                  },
+                  onStepContinue: () {
+                    if (_index <= 0 ) {
+                      _index += 1;
+                    }
+                    if(_index == 1){
+                      isLastStep = true;
+
+                    }
+                    setState(() {});
+
+                  },
+                  onStepTapped: (int index) {
+                    if(index != 1){
+                      isLastStep = false;
+                    }else{
+                      isLastStep = true;
+                    }
+                    setState(() {
+                      _index = index;
+                    });
+                  },
+                  steps: [
+                    Step(
+
+                      title: SmallText(text: 'Personal Details'),
+                      content: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width / 2.7,
+                                child: CustomTextField(
+                                  controller: first_nameCtrl,
+                                  title: 'First Name',
+                                  placeholder: 'John',
+                                ),
+                              ),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width / 2.7,
+                                child: CustomTextField(
+                                  controller: last_nameCtrl,
+                                  title: 'Last Name',
+                                  placeholder: 'Travolta',
+                                ),
+                              ),
+                            ],
+                          ),
+                          CustomTextField(
+                            controller: emailCtrl,
+                            title: 'Your email address',
+                            placeholder: 'johntravolta@gmail.com',
+                          ),
+                          CustomTextField(
+                            controller: passwordCtrl,
+                            isPassword: true,
+                            title: 'Password',
+                            placeholder: '************',
+                          ),
+                          CustomTextField(
+                            controller: password2Ctrl,
+                            isPassword: true,
+                            title: 'Confirm Password',
+                            placeholder: '************',
+                          ),
+                        ],
                       ),
                     ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 2.7,
-                      child: CustomTextField(
-                        controller: last_nameCtrl,
-                        title: 'Last Name',
-                        placeholder: 'Travolta',
+                    Step(
+                      title: SmallText(text: 'Address'),
+                      content: Column(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              CustomTextField(
+                                controller: streetCtrl,
+                                title: 'Street ',
+                                placeholder: '',
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width / 2.7,
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text("Parish"),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Container(
+
+                                          height: 45,
+                                          padding: EdgeInsets.all(5),
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(30),
+                                              color: Colors.white,
+                                              border: Border.fromBorderSide(BorderSide(color:Colors.grey.withOpacity(0.4), width: 1.0))
+                                           ),
+                                          child: DropdownButton(
+                                            value: parishCtrl.text,
+                                            onChanged: (String? value){
+                                              setState(() {
+                                                parishCtrl.text = value!;
+                                              });
+                                            },
+                                            borderRadius: BorderRadius.circular(10),
+                                            underline: SizedBox.shrink(),
+                                            items: parishList.map((parishName){
+                                              return DropdownMenuItem<String>(
+
+                                                  child: Text(parishName, style: TextStyle(fontSize: 14),),
+                                                  value: parishName
+                                              );
+
+                                            }).toList(),
+
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width / 2.7,
+                                    child: CustomTextField(
+                                      controller: cityCtrl,
+                                      title: 'City',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                   ],
-                ),
-                CustomTextField(
-                  controller: emailCtrl,
-                  title: 'Your email address',
-                  placeholder: 'johntravolta@gmail.com',
-                ),
-                CustomTextField(
-                  controller: passwordCtrl,
-                  isPassword: true,
-                  title: 'Password',
-                  placeholder: '************',
-                ),
-                CustomTextField(
-                  controller: password2Ctrl,
-                  isPassword: true,
-                  title: 'Confirm Password',
-                  placeholder: '************',
-                ),
+                  ),
               ],
             ),
           ),
-          Step(
-            title: SmallText(text: 'Address'),
-            content: Column(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    CustomTextField(
-                      controller: streetCtrl,
-                      title: 'Street ',
-                      placeholder: '',
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: Row(
+
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  SmallText(
+                    text: 'Already have an account?',
+                    color: Colors.grey,
+                  ),
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUpPage()));
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(left:8.0),
+                      child: Text('Sign In',
+                          style: TextStyle(fontSize: 14, color:Colors.lightGreen,fontWeight: FontWeight.bold)
+                      ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width / 2.7,
-                          child: CustomTextField(
-                            controller: parishCtrl,
-                            title: 'Parish',
-                          ),
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width / 2.7,
-                          child: CustomTextField(
-                            controller: cityCtrl,
-                            title: 'City',
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+                  )
+                ]
             ),
           ),
         ],
-    ) : Center(child:CircularProgressIndicator(color:AppColors.mainGreen)),
+      ) : Center(child:CircularProgressIndicator(color:AppColors.mainGreen)),
     );
   }
 }
